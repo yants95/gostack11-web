c3c3ad3715101245e4bdf64be7c5baf2dcec1eb1
Yan Soares <yts.1995@gmail.com>
2020-04-23 21:13:22 -0300

    chore: integrating @unform/web at sign un page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
@@ -7,35 +8,41 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background />
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object): void {
+        console.log(data);
+    }
 
-        <Content>
-            <img src={logoImg} alt="GoBarber" />
+    return (
+        <Container>
+            <Background />
 
-            <form>
-                <h1>Faça seu cadastro</h1>
+            <Content>
+                <img src={logoImg} alt="GoBarber" />
 
-                <Input name="name" icon={FiUser} placeholder="Name" />
-                <Input name="email" icon={FiMail} placeholder="E-mail" />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
 
-                <Input
-                    name="password"
-                    icon={FiLock}
-                    type="password"
-                    placeholder="Senha"
-                />
+                    <Input name="name" icon={FiUser} placeholder="Name" />
+                    <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-                <Button type="submit">Cadastrar</Button>
-            </form>
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        type="password"
+                        placeholder="Senha"
+                    />
 
-            <a href="login">
-                <FiArrowLeft />
-                Voltar para logon
-            </a>
-        </Content>
-    </Container>
-);
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+
+                <a href="login">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;
