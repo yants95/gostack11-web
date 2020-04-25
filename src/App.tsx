bfd811081ec0ba94e76003cadde723f59047e57b
Yan Soares <yts.1995@gmail.com>
2020-04-24 22:00:02 -0300

    feat: integrating context to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
     <>
-        <SignUp />
+        <AuthContext.Provider value={{ name: 'Yan' }}>
+            <SignIn />
+        </AuthContext.Provider>
+
         <GlobalStyle />
     </>
 );
