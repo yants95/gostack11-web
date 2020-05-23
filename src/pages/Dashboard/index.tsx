3fa3d441dfdd2988e494fcd3eee76437b95a7357
Yan Soares <yts.1995@gmail.com>
2020-05-23 12:34:22 -0300

    feat: list of appointments bookeds

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    NextAppointment,
+    Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img
+                                src="https://avatars0.githubusercontent.com/u/14876382?s=460&u=35101d5d3eb4c048caaadd8848886bbc8c2d0cbe&v=4"
+                                alt="Yan Soares"
+                            />
+
+                            <strong>Yan Soares</strong>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+                </Schedule>
+                <Calendar />
+            </Content>
         </Container>
     );
 };
