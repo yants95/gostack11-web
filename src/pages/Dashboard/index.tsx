4eee04c080375157689a708788b99de3c871e717
Yan Soares <yts.1995@gmail.com>
2020-05-24 12:27:15 -0300

    feat: disabled click on calendar disabled days

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
-import { isToday, format, parseISO } from 'date-fns';
+import { isToday, format, parseISO, isAfter } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
@@ -50,7 +50,7 @@ const Dashboard: React.FC = () => {
 
     const handleDateChange = useCallback(
         (day: Date, modifiers: DayModifiers) => {
-            if (modifiers.available) {
+            if (modifiers.available && !modifiers.disabled) {
                 setSelectedDate(day);
             }
         },
@@ -125,6 +125,12 @@ const Dashboard: React.FC = () => {
         });
     }, [appointments]);
 
+    const nextAppointment = useMemo(() => {
+        return appointments.find((appointment) =>
+            isAfter(parseISO(appointment.date), new Date()),
+        );
+    }, [appointments]);
+
     return (
         <Container>
             <Header>
@@ -154,24 +160,30 @@ const Dashboard: React.FC = () => {
                         <span>{selectedWeekDay}</span>
                     </p>
 
-                    <NextAppointment>
-                        <strong>Atendimento a seguir</strong>
-                        <div>
-                            <img
-                                src="https://avatars0.githubusercontent.com/u/14876382?s=460&u=35101d5d3eb4c048caaadd8848886bbc8c2d0cbe&v=4"
-                                alt="Yan Soares"
-                            />
+                    {isToday(selectedDate) && nextAppointment && (
+                        <NextAppointment>
+                            <strong>Agendamento a seguir</strong>
+                            <div>
+                                <img
+                                    src={nextAppointment.user.avatar_url}
+                                    alt={nextAppointment.user.name}
+                                />
 
-                            <strong>Yan Soares</strong>
-                            <span>
-                                <FiClock />
-                                08:00
-                            </span>
-                        </div>
-                    </NextAppointment>
+                                <strong>{nextAppointment.user.name}</strong>
+                                <span>
+                                    <FiClock />
+                                    {nextAppointment.hourFormatted}
+                                </span>
+                            </div>
+                        </NextAppointment>
+                    )}
 
                     <Section>
                         <strong>Manhã</strong>
+
+                        {morningAppointments.length === 0 && (
+                            <p>Nenhum agendamento neste período</p>
+                        )}
 
                         {morningAppointments.map((appointment) => (
                             <Appointment key={appointment.id}>
@@ -194,6 +206,10 @@ const Dashboard: React.FC = () => {
 
                     <Section>
                         <strong>Tarde</strong>
+
+                        {afternoonAppointments.length === 0 && (
+                            <p>Nenhum agendamento neste período</p>
+                        )}
 
                         {afternoonAppointments.map((appointment) => (
                             <Appointment key={appointment.id}>
